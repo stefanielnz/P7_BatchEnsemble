fdafd5b698870163690e26d597a02dd4099dc1ea
Stefanie Lanz <stefanie.lanz@tum.de>
2024-12-02 16:21:35 +0100

    sgd

--- code/submit_test.sh
+++ code/submit_test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ### General options
 ### -- specify queue --
-#BSUB -q gpua10
+#BSUB -q gpua100
 ### -- set the job Name --
 #BSUB -J Test_Python
 ### -- ask for number of cores (default: 1) --
